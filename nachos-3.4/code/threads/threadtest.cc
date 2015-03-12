f20aaa57ee86b242a8b53f70e07bb9a322d83065
Ian Callaway <ipc0385@louisiana.edu>
2015-03-12 18:23:15 -0500

    first commit

---
+++ nachos-3.4/code/threads/threadtest.cc
@@ -0,0 +1,54 @@
+// threadtest.cc 
+//	Simple test case for the threads assignment.
+//
+//	Create two threads, and have them context switch
+//	back and forth between themselves by calling Thread::Yield, 
+//	to illustratethe inner workings of the thread system.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "system.h"
+
+//----------------------------------------------------------------------
+// SimpleThread
+// 	Loop 5 times, yielding the CPU to another ready thread 
+//	each iteration.
+//
+//	"which" is simply a number identifying the thread, for debugging
+//	purposes.
+//----------------------------------------------------------------------
+
+void
+SimpleThread(int which)
+{
+	printf("\n(%d", which);
+	printf(";%d)", currentThread->Cycle());
+}
+
+
+//----------------------------------------------------------------------
+// ThreadTest
+// 	Invoke a test routine.
+//----------------------------------------------------------------------
+
+void
+ThreadTest()
+{
+    DEBUG('t', "Entering ThreadTest");
+	
+    currentThread->Cycle();
+   	(new Thread("forked thread"))->Fork(SimpleThread, 1);
+   	(new Thread("forked thread"))->Fork(SimpleThread, 2);
+   	(new Thread("forked thread"))->Fork(SimpleThread, 4);
+   	(new Thread("forked thread"))->Fork(SimpleThread, 5);
+   	(new Thread("forked thread"))->Fork(SimpleThread, 6);
+	
+	Thread *ptr = new Thread("child", 4);
+	ptr->Fork(SimpleThread, 3);
+   	ptr = ptr->GetParent();
+	printf("parent: %d", ptr->myID);
+}
+
---
+++ nachos-3.4/code/userprog/addrspace.cc
@@ -0,0 +1,240 @@
+// addrspace.cc 
+//	Routines to manage address spaces (executing user programs).
+//
+//	In order to run a user program, you must:
+//
+//	1. link with the -N -T 0 option 
+//	2. run coff2noff to convert the object file to Nachos format
+//		(Nachos object code format is essentially just a simpler
+//		version of the UNIX executable object code format)
+//	3. load the NOFF file into the Nachos file system
+//		(if you haven't implemented the file system yet, you
+//		don't need to do this last step)
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "system.h"
+#include "addrspace.h"
+#include "noff.h"
+#ifdef HOST_SPARC
+#include <strings.h>
+#endif
+#include "bitmap.h"
+//----------------------------------------------------------------------
+// SwapHeader
+// 	Do little endian to big endian conversion on the bytes in the 
+//	object file header, in case the file was generated on a little
+//	endian machine, and we're now running on a big endian machine.
+//----------------------------------------------------------------------
+
+static void 
+SwapHeader (NoffHeader *noffH)
+{
+	noffH->noffMagic = WordToHost(noffH->noffMagic);
+	noffH->code.size = WordToHost(noffH->code.size);
+	noffH->code.virtualAddr = WordToHost(noffH->code.virtualAddr);
+	noffH->code.inFileAddr = WordToHost(noffH->code.inFileAddr);
+	noffH->initData.size = WordToHost(noffH->initData.size);
+	noffH->initData.virtualAddr = WordToHost(noffH->initData.virtualAddr);
+	noffH->initData.inFileAddr = WordToHost(noffH->initData.inFileAddr);
+	noffH->uninitData.size = WordToHost(noffH->uninitData.size);
+	noffH->uninitData.virtualAddr = WordToHost(noffH->uninitData.virtualAddr);
+	noffH->uninitData.inFileAddr = WordToHost(noffH->uninitData.inFileAddr);
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::AddrSpace
+// 	Create an address space to run a user program.
+//	Load the program from a file "executable", and set everything
+//	up so that we can start executing user instructions.
+//
+//	Assumes that the object code file is in NOFF format.
+//
+//	First, set up the translation from program memory to physical 
+//	memory.  For now, this is really simple (1:1), since we are
+//	only uniprogramming, and we have a single unsegmented page table
+//
+//	"executable" is the file containing the object code to load into memory
+//----------------------------------------------------------------------
+
+//main memory bitmap
+static BitMap mainMemoryBits(32);
+
+
+int BestFit(unsigned frames) {
+	int bit = 32, size;
+	for(int i = 0, j; i < 32; i += j) {
+		i = mainMemoryBits.FreeSearch(i);
+		if((j = mainMemoryBits.FreeCount(i)) < size) {
+			bit = i;
+			size = j;
+		}
+	}
+	return bit;
+}
+
+int WorstFit(int frames) {
+	int bit = 32, size;
+	for(int i = 0, j; i < 32; i += j) {
+		i = mainMemoryBits.FreeSearch(i);
+		if((j = mainMemoryBits.FreeCount(i)) > size) {
+			bit = i;
+			size = j;
+		}
+	}
+	return bit;
+}
+
+int FirstFit(int frames) {
+	for(int i = 0, j; i < 32; i += j) {
+		i = mainMemoryBits.FreeSearch(i);
+		if((j = mainMemoryBits.FreeCount(i)) <= frames) return i;
+	}
+	return 32;
+}
+
+AddrSpace::AddrSpace(OpenFile *executable)
+{
+	mainMemoryBits.PrintBits();
+//idk what this does
+    NoffHeader noffH;
+	
+//i is something,
+//size is like all the memory in bytes we're allocating for this
+    unsigned int i, size;
+
+//ignore this wizardry
+    executable->ReadAt((char *)&noffH, sizeof(noffH), 0);
+    if ((noffH.noffMagic != NOFFMAGIC) && 
+		(WordToHost(noffH.noffMagic) == NOFFMAGIC))
+    	SwapHeader(&noffH);
+    ASSERT(noffH.noffMagic == NOFFMAGIC);
+
+//this is the address space in bytes
+    size =
+//size of the code that we have for some reason
+		noffH.code.size
+//size of the data that we have for some readon
+		+ noffH.initData.size
+//size of idk
+		+ noffH.uninitData.size
+//size of the stack
+		+ UserStackSize;
+
+//the address space will be divided into pages
+    numPages = divRoundUp(size, PageSize);
+
+//size is the address space in bytes but rounded up to a page
+    size = numPages * PageSize;
+
+//if there's not enough memory we kill this
+    ASSERT(numPages <= NumPhysPages);
+
+    DEBUG('a', "Initializing address space, num pages %d, size %d\n", numPages, size);
+
+	unsigned offset = FirstFit(numPages);
+	for(unsigned j = 0; j < numPages; j++)
+		mainMemoryBits.Mark(j);
+
+// first, set up the translation
+    pageTable = new TranslationEntry[numPages];
+    for (i = 0; i < numPages; i++) {
+		pageTable[i].virtualPage = i;	// for now, virtual page # = phys page #
+		pageTable[i].physicalPage = i + offset;
+		pageTable[i].valid = TRUE;
+		pageTable[i].use = FALSE;
+		pageTable[i].dirty = FALSE;
+		pageTable[i].readOnly = FALSE;  // if the code segment was entirely on 
+					// a separate page, we could set its 
+					// pages to be read-only
+    }
+    
+// zero out the entire address space, to zero the unitialized data segment 
+// and the stack segment
+    bzero(machine->mainMemory + offset, size + offset);
+
+// then, copy in the code and data segments into memory
+    if (noffH.code.size > 0) {
+        DEBUG('a', "Initializing code segment, at 0x%x, size %d\n", 
+			noffH.code.virtualAddr, noffH.code.size);
+        executable->ReadAt(&(machine->mainMemory[noffH.code.virtualAddr + (offset * PageSize)]),
+			noffH.code.size, noffH.code.inFileAddr);
+    }
+    if (noffH.initData.size > 0) {
+        DEBUG('a', "Initializing data segment, at 0x%x, size %d\n", 
+			noffH.initData.virtualAddr, noffH.initData.size);
+        executable->ReadAt(&(machine->mainMemory[noffH.initData.virtualAddr + (offset * PageSize)]),
+			noffH.initData.size, noffH.initData.inFileAddr);
+    }
+	mainMemoryBits.PrintBits();
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::~AddrSpace
+// 	Dealloate an address space.  Nothing for now!
+//----------------------------------------------------------------------
+
+AddrSpace::~AddrSpace()
+{
+   delete pageTable;
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::InitRegisters
+// 	Set the initial values for the user-level register set.
+//
+// 	We write these directly into the "machine" registers, so
+//	that we can immediately jump to user code.  Note that these
+//	will be saved/restored into the currentThread->userRegisters
+//	when this thread is context switched out.
+//----------------------------------------------------------------------
+
+void
+AddrSpace::InitRegisters()
+{
+    int i;
+
+    for (i = 0; i < NumTotalRegs; i++)
+	machine->WriteRegister(i, 0);
+
+    // Initial program counter -- must be location of "Start"
+    machine->WriteRegister(PCReg, 0);	
+
+    // Need to also tell MIPS where next instruction is, because
+    // of branch delay possibility
+    machine->WriteRegister(NextPCReg, 4);
+
+   // Set the stack register to the end of the address space, where we
+   // allocated the stack; but subtract off a bit, to make sure we don't
+   // accidentally reference off the end!
+    machine->WriteRegister(StackReg, numPages * PageSize - 16);
+    DEBUG('a', "Initializing stack register to %d\n", numPages * PageSize - 16);
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::SaveState
+// 	On a context switch, save any machine state, specific
+//	to this address space, that needs saving.
+//
+//	For now, nothing!
+//----------------------------------------------------------------------
+
+void AddrSpace::SaveState() 
+{}
+
+//----------------------------------------------------------------------
+// AddrSpace::RestoreState
+// 	On a context switch, restore the machine state so that
+//	this address space can run.
+//
+//      For now, tell the machine where to find the page table.
+//----------------------------------------------------------------------
+
+void AddrSpace::RestoreState() 
+{
+    machine->pageTable = pageTable;
+    machine->pageTableSize = numPages;
+}
