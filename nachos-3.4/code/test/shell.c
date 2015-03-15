d28ec4c4f5f7766c5518f8094c18460ebaf9ac28
Ian Callaway <ipc0385@louisiana.edu>
2015-03-15 18:18:36 -0500

    Editted to test out Exec and Joins

--- nachos-3.4/code/test/shell.c
+++ nachos-3.4/code/test/shell.c
@@ -3,7 +3,12 @@
 int
 main()
 {
-    SpaceId newProc;
+	SpaceId myProcess;
+	myProcess = Exec("../test/sort");
+	Join(myProcess);
+	/*SpaceId myProcess;
+	myProcess = Exec("../test/shell");
+    /*SpaceId newProc;
     OpenFileId input = ConsoleInput;
     OpenFileId output = ConsoleOutput;
     char prompt[2], ch, buffer[60];
@@ -14,22 +19,23 @@ main()
 
     while( 1 )
     {
-	Write(prompt, 2, output);
+		Write(prompt, 2, output);
 
-	i = 0;
+		i = 0;
 	
-	do {
+		do {
 	
-	    Read(&buffer[i], 1, input); 
+		    Read(&buffer[i], 1, input); 
 
-	} while( buffer[i++] != '\n' );
+		} while( buffer[i++] != '\n' );
 
-	buffer[--i] = '\0';
+		buffer[--i] = '\0';
 
-	if( i > 0 ) {
-		newProc = Exec(buffer);
-		Join(newProc);
-	}
-    }
+		if( i > 0 ) {
+			newProc = Exec(buffer);
+			Join(newProc);
+		}
+    }*/
+	return 0;
 }
 
