4cf7b30843a7336ac1c6c7d7de846d2002686409
Ian Callaway <ipc0385@louisiana.edu>
2015-03-16 10:12:43 -0500

    Addrspace has error code variable

--- nachos-3.4/code/userprog/addrspace.h
+++ nachos-3.4/code/userprog/addrspace.h
@@ -31,7 +31,7 @@ class AddrSpace {
     void SaveState();			// Save/restore address space-specific
     void RestoreState();		// info on a context switch 
 
-	int valid;
+	int code;
 
   private:
     TranslationEntry *pageTable;	// Assume linear page table translation
